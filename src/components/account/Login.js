d970745954ed8f8308b4b5fb5d02d64aa74d00f9
mukeshWebGit <mmuskan.940@gmail.com>
2025-04-23 16:34:17 +0530

    navigate

--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import {Box, TextField, Button, DialogTitle, styled, Typography } from '@mui/material';  
 import { API } from '../../services/api.js';
 import { DataContext } from '../../context/dataprovider.js';
+import { useNavigate } from 'react-router-dom';
 
 const Wrapper = styled(Box)`
 width:400px; border:1px solid #ddd; box-shadow:3px 2px 3px  rgba(0, 0, 0, .15); margin: 100px auto 0; text-transform:none; 
@@ -30,6 +31,7 @@ export default function Login() {
    const [signin, setSignin] = useState(signinInitiallValue);  
    const [error, setError] = useState('');
    const {setAccount } = useContext(DataContext);
+   const navigate = useNavigate();
 
 const SwitchAccount = () => {
    account === 'signUp' ? toggleAccount('Login') : toggleAccount('signUp');
@@ -64,6 +66,7 @@ const signupUser = async  () => {
       sessionStorage.setItem('accessToken', `Bearer ${response.data.accessToken}`);
       sessionStorage.setItem('refreshToken', `Bearer ${response.data.refreshToken}`);
       setAccount({username: response.data.username, name:response.data.name});
+      navigate('/home');
     }else{
       setError('Someting went wrong! please try again later');
     }
