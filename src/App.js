699bbf4842710b5597b248e0ad347823073aa412
mukeshWebGit <mmuskan.940@gmail.com>
2025-04-23 13:52:33 +0530

    added route

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import Login from './components/account/Login';
 import Home from './components/home/home';
 import { DataProvider } from './context/dataprovider';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <DataProvider>
-       <Login/>
-       <Home/>
+        <BrowserRouter>
+        <Routes>
+         <Route path='/login' element={ <Login/>} />  
+         <Route path='/' element={ <Home/>} /> 
+          </Routes>
+       </BrowserRouter>
        </DataProvider>
     </div>
   );
