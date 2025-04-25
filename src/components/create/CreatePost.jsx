15ffd98af62287ab8f7acec96e4b648f9d1d5fe0
mukeshWebGit <mmuskan.940@gmail.com>
2025-04-25 17:02:30 +0530

    created add post form

--- src/components/create/CreatePost.jsx
+++ src/components/create/CreatePost.jsx
@@ -1,4 +1,4 @@
-import { Box, styled } from '@mui/material';
+import { Box, Button, FormControl, InputBase, styled } from '@mui/material';
 import React from 'react';
 const Container = styled(Box)`
     width: 100%;
@@ -6,23 +6,59 @@ const Container = styled(Box)`
     max-width: 1200px;
     margin: 0 auto;
 `;
-
+const StyledFormControl = styled(FormControl)`
+    display: flex;  
+    flex-direction: column;
+    width: 100%;
+    margin: 20px 0;
+    & > div {
+        margin: 10px 0;
+    }
+    & > div > label {
+        margin-bottom: 5px;
+    }
+    & > div > input[type="file"] {
+        padding: 10px;
+    }
+    & > div > textarea {
+        width: 100%;
+        height: 200px;
+        padding: 10px;
+        border-radius: 4px;
+        border: 1px solid #ccc;
+    }
+    & > button {
+        background-color: #3f51b5;
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 4px;
+        cursor: pointer;
+        &:hover {
+            background-color: #303f9f;
+        }
+    }
+`;
 const CreatePost = () => {
     return (
         <div>
             <Container>
             <h1>Create a New Post</h1>
-            <form>
+            <StyledFormControl>
                 <div>
-                    <label htmlFor="title">Title:</label>
-                    <input type="text" id="title" name="title" />
+                    <label htmlFor="uploadImg">upload</label>
+                    <input type="file" id="uploadImg" name="uploadImg" />
+                </div>
+                <div>
+                    <label htmlFor="title">upload</label>
+                    <InputBase placeholder='Title'></InputBase> 
                 </div>
                 <div>
                     <label htmlFor="content">Content:</label>
                     <textarea id="content" name="content"></textarea>
                 </div>
-                <button type="submit">Submit</button>
-            </form>
+                <Button type="submit">Submit</Button>
+            </StyledFormControl>
             </Container>
         </div>
     );
